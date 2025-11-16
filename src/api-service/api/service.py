14f2a1420b43e9f24e977a9e67b53d7e2614013d
shivasj <shivasj@gmail.com>
2025-11-16 09:16:15 -0500

    image chat fix

--- src/api-service/api/service.py
+++ src/api-service/api/service.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+
 from api.routers import newsletter, podcast
 
 from api.routers import llm_chat, llm_cnn_chat
@@ -26,11 +27,17 @@ async def get_index():
     return {"message": "Welcome to AC215"}
 
 
+@app.get("/square_root/")
+async def square_root(x: float = 1, y: float = 2):
+    z = x**2 + y**2
+    return z**0.5
+
+
 # Additional routers here
-# app.include_router(newsletter.router, prefix="/newsletters")
-# app.include_router(podcast.router, prefix="/podcasts")
-# app.include_router(llm_chat.router, prefix="/llm")
-# app.include_router(llm_cnn_chat.router, prefix="/llm-cnn")
+app.include_router(newsletter.router, prefix="/newsletters")
+app.include_router(podcast.router, prefix="/podcasts")
+app.include_router(llm_chat.router, prefix="/llm")
+app.include_router(llm_cnn_chat.router, prefix="/llm-cnn")
 # app.include_router(llm_rag_chat.router, prefix="/llm-rag")
 # app.include_router(llm_agent_chat.router, prefix="/llm-agent")
 # app.include_router(test_router.router, prefix="/test")
